d693998f40312c9d88f64311dc32d118deac3ccd
vikram7684 <vikramveer@outlook.com>
2021-11-01 00:59:26 -0400

    oddEven Program

---
+++ src/oddEven.java
@@ -0,0 +1,12 @@
+public class oddEven {
+    public static void main(String[] args) {
+        int a = 7;
+        if(a%2==0){
+            System.out.println("Even");}
+            else{
+            System.out.println("Odd");
+
+            }
+        }
+    }
+
